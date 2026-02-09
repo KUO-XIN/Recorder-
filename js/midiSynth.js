c8ed6a8ad52acd076ded4f2f43bfd996055a3b08
KUO-XIN <s112321015@mail1.ncnu.edu.tw>
2026-02-09 13:08:03 +0800

    Update monoSynth to use C3 octave and adjust settings

--- js/midiSynth.js
+++ js/midiSynth.js
@@ -4,32 +4,32 @@
 window.monoSynth = new Tone.Sampler({
     urls: {
         // ====== Octave 4 ======
-        "C4": "C4.wav",
-        "C#4": "H-C4.wav",
-        "D4": "D4.wav",
-        "D#4": "H-D4.wav",
-        "E4": "E4.wav",
-        "F4": "F4.wav",
-        "F#4": "H-F4.wav",
-        "G4": "G4.wav",
-        "G#4": "H-G4.wav",
-        "A4": "A4.wav",
-        "A#4": "H-A4.wav",
-        "B4": "B4.wav",
+        "C3": "C3.wav",
+        "C#3": "H-C3.wav",
+        "D3": "D3.wav",
+        "D#3": "H-D3.wav",
+        "E3": "E3.wav",
+        "F3": "F3.wav",
+        "F#3": "H-F3.wav",
+        "G3": "G3.wav",
+        "G#3": "H-G3.wav",
+        "A3": "A3.wav",
+        "A#3": "H-A3.wav",
+        "B3": "B3.wav",
 
         // ====== Octave 5 ======
-        "C5": "C5.wav",
-        "D5": "D5.wav",
-        "E5": "E5.wav",
-        "F5": "F5.wav",
-        "G5": "G5.wav",
-        "A5": "A5.wav",
-        "B5": "B5.wav"
+        "C4": "C4.wav",
+        "D4": "D4.wav",
+        "E4": "E4.wav",
+        "F4": "F4.wav",
+        "G4": "G4.wav",
+        "A4": "A4.wav",
+        "B4": "B4.wav"
     },
 
     baseUrl: "./Recorder-result/",
-    attack: 0.01,
-    release: 0.35
+    attack: 0.05,
+    release: 0.8
 }).toDestination();
 
 window.recorderFilter = new Tone.Filter({
@@ -46,20 +46,20 @@ window.monoSynth.connect(window.recorderFilter);
 // 音名對照
 // =========================
 export const NOTE_TO_MIDI = {
+    C3: 48, D3: 50, E3: 52, F3: 53,
+    G3: 55, A3: 57, B3: 59,
     C4: 60, D4: 62, E4: 64, F4: 65,
     G4: 67, A4: 69, B4: 71,
-    C5: 72, D5: 74, E5: 76, F5: 77,
-    G5: 79, A5: 81, B5: 83,
-    "H-C4": 61, "H-D4": 63, "H-F4": 66,
-    "H-G4": 68, "H-A4": 70
+    "H-C3": 49, "H-D3": 51, "H-F3": 54,
+    "H-G3": 56, "H-A3": 58
 };
 
 const TONE_NOTE_MAP = {
-    "H-C4": "C#4",
-    "H-D4": "D#4",
-    "H-F4": "F#4",
-    "H-G4": "G#4",
-    "H-A4": "A#4"
+    "H-C3": "C#3",
+    "H-D3": "D#3",
+    "H-F3": "F#3",
+    "H-G3": "G#3",
+    "H-A3": "A#3"
 };
 
 // =========================
@@ -121,6 +121,3 @@ export function setInstrumentVolume(v) {
     recorderFilter.frequency.value =
         900 + curved * 2800;
 }
-
-
-
