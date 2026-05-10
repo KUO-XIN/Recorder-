f3275a14063cc5b2dcbe87c78b5df6d939c169b5
KUO-XIN <s112321015@mail1.ncnu.edu.tw>
2026-05-10 14:09:32 +0800

    Update monoSynth and NOTE_TO_MIDI mappings

--- js/midiSynth.js
+++ js/midiSynth.js
@@ -4,27 +4,27 @@
 window.monoSynth = new Tone.Sampler({
     urls: {
         // ====== Octave 4 ======
-        "C3": "C3.wav",
-        "C#3": "H-C3.wav",
-        "D3": "D3.wav",
-        "D#3": "H-D3.wav",
-        "E3": "E3.wav",
-        "F3": "F3.wav",
-        "F#3": "H-F3.wav",
-        "G3": "G3.wav",
-        "G#3": "H-G3.wav",
-        "A3": "A3.wav",
-        "A#3": "H-A3.wav",
-        "B3": "B3.wav",
-
-        // ====== Octave 5 ======
         "C4": "C4.wav",
+        "C#4": "H-C4.wav",
         "D4": "D4.wav",
+        "D#4": "H-D4.wav",
         "E4": "E4.wav",
         "F4": "F4.wav",
-        "G4": "G4.wav",
+        "F#4": "H-F4.wav",
+        "G4": "G3.wav",
+        "G#4": "H-G4.wav",
         "A4": "A4.wav",
-        "B4": "B4.wav"
+        "A#4": "H-A4.wav",
+        "B4": "B4.wav",
+
+        // ====== Octave 5 ======
+        "C5": "C5.wav",
+        "D5": "D5.wav",
+        "E5": "E5.wav",
+        "F5": "F5.wav",
+        "G5": "G5.wav",
+        "A5": "A5.wav",
+        "B5": "B5.wav"
     },
 
     baseUrl: "./Recorder-result/",
@@ -46,20 +46,20 @@ window.monoSynth.connect(window.recorderFilter);
 // 音名對照
 // =========================
 export const NOTE_TO_MIDI = {
-    C3: 48, D3: 50, E3: 52, F3: 53,
-    G3: 55, A3: 57, B3: 59,
     C4: 60, D4: 62, E4: 64, F4: 65,
     G4: 67, A4: 69, B4: 71,
-    "H-C3": 49, "H-D3": 51, "H-F3": 54,
-    "H-G3": 56, "H-A3": 58
+    C4: 72, D4: 74, E4: 76, F4: 77,
+    G4: 79, A4: 81, B4: 83,
+    "H-C3": 61, "H-D3": 63, "H-F3": 66,
+    "H-G3": 68, "H-A3": 70
 };
 
 const TONE_NOTE_MAP = {
-    "H-C3": "C#3",
-    "H-D3": "D#3",
-    "H-F3": "F#3",
-    "H-G3": "G#3",
-    "H-A3": "A#3"
+    "H-C4": "C#4",
+    "H-D4": "D#4",
+    "H-F4": "F#4",
+    "H-G4": "G#4",
+    "H-A4": "A#4"
 };
 
 // =========================
