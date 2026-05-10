050465a9f2c9c990208ccdc8f5da92719d060c9e
KUO-XIN <s112321015@mail1.ncnu.edu.tw>
2026-05-10 16:55:38 +0800

    Add functionality to play next MIDI note

--- js/maina.js
+++ js/maina.js
@@ -58,6 +58,10 @@ midiFileInput.addEventListener("change", async (event) => {
     await midiPlayer.loadFile(file);
 });
 
+document.getElementById("next").onclick = () => {
+    midiPlayer.playNextNote();
+};
+
 /* ================= Hands ================= */
 let hands = new Hands({
     locateFile: f => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${f}`
